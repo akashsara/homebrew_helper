9a2f94698198a20fbd448df3dff47adeee827191
Akash Saravanan <akashsara@outlook.com>
2021-10-21 17:01:02 -0600

    Just some fun commands to play around with.

--- config.py
+++ config.py
@@ -20,3 +20,49 @@ COIN_TOSS_MAX_TOSSES = 20
 INITIATIVE_MAX_NPCS = 20
 DICE_ROLL_MAX_DICE = 50
 DICE_ROLL_LARGEST_DIE = 100
+
+RICK_ROLL_LYRICS = [
+    [
+        "♫♫♫ We're no strangers to love ♫♫♫",
+        "♫♫♫ You know the rules and so do I ♫♫♫",
+    ],
+    [
+        "♫♫♫ A full commitment's what I'm thinking of ♫♫♫",
+        "♫♫♫ You wouldn't get this from any other guy ♫♫♫",
+    ],
+    [
+        "♫♫♫ I just wanna tell you how I'm feeling ♫♫♫",
+        "♫♫♫ Gotta make you understand ♫♫♫",
+    ],
+    [
+        "♫♫♫ Never gonna give you up ♫♫♫",
+        "♫♫♫ Never gonna let you down ♫♫♫",
+        "♫♫♫ Never gonna run around and desert you ♫♫♫",
+        "♫♫♫ Never gonna make you cry ♫♫♫",
+        "♫♫♫ Never gonna say goodbye ♫♫♫",
+        "♫♫♫ Never gonna tell a lie and hurt you ♫♫♫",
+    ],
+]
+
+ORACLE_ANSWERS = [
+    "HELL YEAH",
+    "Yes.",
+    "That is an excellent question.",
+    "Wouldn't you like to know, weather boy?",
+    "No.",
+    "Nope.",
+    "Nah man.",
+    "Yup yup!",
+    "Maybe",
+    "idk lol",
+    "Things will make sense when they make sense.",
+    "It's all part of the plan",
+    "It is known.",
+    "No chance.",
+    "Yes, 100%.",
+    "Ask again later.",
+    "The Oracle you're trying to reach is currently unavailable. Please hold the line or try again later.",
+    "Don't count on it.",
+    "My sources say no.",
+    "Don't try to play the fool with me Nikesh."
+]
