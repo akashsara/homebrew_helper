e9aa8e9a3567e3d966c888da7975608207a55ab0
akashsara <akashsara@outlook.com>
2023-06-08 01:31:37 -0600

    Refactor(utils/gen_utils.py): Add type hinting; remove unused code.

--- utils/gen_utils.py
+++ utils/gen_utils.py
@@ -1,11 +1,7 @@
-import os
 import re
 import sys
 import uuid
-from collections import defaultdict
-from functools import partial
-
-import joblib
+from typing import Dict, List
 
 sys.path.append("../")
 import logging
@@ -13,41 +9,35 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def generate_id():
+def generate_id() -> str:
     return str(uuid.uuid4())
 
 
-def generate_unique_id(generated):
+def generate_unique_id(generated: set) -> str:
     id_ = generate_id()
     while id_ in generated:
         id_ = generate_id()
     return id_
 
 
-def save_file(data, file_path):
-    logger.info(f"Saving to {file_path}")
-    joblib.dump(data, file_path)
-
-
-def get_default_user():
+def get_default_user() -> Dict:
     return {"active": None, "characters": []}
 
 
-def discord_name_to_id(name):
+def discord_name_to_id(name: str) -> str:
     search = re.findall("\d+", name)
     logger.info(f"Found {search} from {name}.")
     if search:
         return str(search[0])
-    return None
 
 
-def pad(text_to_pad, length_to_pad_to, direction):
+def pad(text_to_pad: str, length_to_pad_to: int, direction: str) -> str:
     if direction == "left":
         return (" " * (length_to_pad_to - len(text_to_pad))) + text_to_pad
     return text_to_pad + (" " * (length_to_pad_to - len(text_to_pad)))
 
 
-def format_rolls(rolls):
+def format_rolls(rolls: List[str]) -> str:
     new_roll = []
     for item in rolls:
         key, value = item
@@ -59,7 +49,7 @@ def format_rolls(rolls):
     return "\n".join(new_roll)
 
 
-def format_repeated_rolls(rolls):
+def format_repeated_rolls(rolls: List[Dict]) -> str:
     all_text = []
     for item in rolls:
         roll = item["user_roll"]
